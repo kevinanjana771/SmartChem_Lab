d5973c78f8600a33809629fe4c6c66cce81786fa
kevinanjana771 <kevinanjana771@gmail.com>
2026-02-23 09:51:24 +0530

    added the new folder structure

---
+++ SmartChem_backend/routes/practicalRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  getAllPracticals,
+  getPracticalById,
+  getPracticalSteps,
+  getPracticalEquipments 
+} from "../controllers/practicalController.js";
+
+const router = express.Router();
+
+
+//GET ALL PRACTICALS
+//GET /api/practicals
+
+router.get("/", getAllPracticals);
+
+
+//GET SINGLE PRACTICAL
+//GET /api/practicals/:id
+
+router.get("/:id", getPracticalById);
+
+
+//GET STEPS FOR PRACTICAL
+//GET /api/practicals/:id/steps
+
+router.get("/:id/steps", getPracticalSteps);
+
+
+//GET EUIPMENTS FOR PRACTICAL (RESTORED)
+//GET /api/practicals/:id/equipments
+
+router.get("/:id/equipments", getPracticalEquipments);
+
+export default router;
